05b43599d844cbb735eb1aee84e688227dfeef8a
Mattia Peano <153503139+unipeano@users.noreply.github.com>
2025-05-23 14:47:11 +0200

    Initial commit

---
+++ src/test/java/unito/tweb/projectbackend/ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unito.tweb.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
